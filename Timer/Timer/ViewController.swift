cd17939bab074f94561b40081c91f9dbb358f3ad
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-09-08 18:13:44 +0300

    Update

--- Timer/Timer/ViewController.swift
+++ Timer/Timer/ViewController.swift
@@ -19,9 +19,10 @@ class ViewController: UIViewController {
     
     // TR : Tetiklenecek Fonksiyon.
     // EN : Function to be triggered.
-    @objc func TimerFunction(){
+    @objc func counter(){
         
         seconds += 1
+        
         if(seconds > 59){
             minutes += 1
             seconds = 0
@@ -30,17 +31,19 @@ class ViewController: UIViewController {
         if(minutes > 59){
             hours += 1
             minutes = 0
-        }        
+        }
+        
         LabelTimer.text = "\(hours):\(minutes):\(seconds)"
+        
     }
 
     @IBAction func start(_ sender: Any) {
-        // TR : Zamanlayici nesnesi olusturulur.
-        // EN : The timer object is initialized.
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerFunction), userInfo: nil,repeats: true)
+        // TR : Zamanlayici nesnesini baslat.
+        // EN : Initialize the timer object.
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(counter), userInfo: nil,repeats: true)
     }
+    
     @IBAction func reset(_ sender: UIButton) {
-        
         // TR : Sayaci sonlandir.
         // EN : Terminate the counter.
         timer.invalidate()
@@ -50,4 +53,3 @@ class ViewController: UIViewController {
         LabelTimer.text = "\(hours):\(minutes):\(seconds)"
     }
 }
-
